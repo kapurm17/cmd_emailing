db513fde9d60cf0cdc17c0222ad300f19e2887dc
mayank-kapur <kapurm17@gmail.com>
2018-11-21 20:52:32 +0530

    Update e_mailer.py

--- e_mailer.py
+++ e_mailer.py
@@ -8,12 +8,10 @@ try:
     print("SERVER CONNECTED")
 except:
     print("Could Not connect to Gmail")             #in case of failure
-
 user = input("Enter User id\n")                     #YOUR ID
 Pass_w = input("\nEnter your Password\n")           #YOUR Password
 reciever_id = input("\nEnter reciever id\n")        #Reciever ID
 msg = input("\nEnter message\n")                        #message
-
 try:
     server.login(user, Pass_w)                      #user log in
     print("User Logged in")
@@ -21,18 +19,8 @@ except:
     print("Allow Less secure apps in GOOGLE ACCOUNT SETTINGS to use SMTP services")
     server.quit()
     exit()
-
 server.sendmail(user, reciever_id, msg)
 print("MAIL sent")                                  #confirmation
-
-
 print("Closing Connection")
 server.quit()                                       #closing server connection
 print("Server closed")
-
-
-
-
-
-
-
